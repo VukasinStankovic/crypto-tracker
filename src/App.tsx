a5c74cdf27c4041ac773f824a2c6c361a7cec056
Vukasin Stankovic <vule.stankovic2@gmail.com>
2023-10-18 22:15:22 +0200

    Create initial file structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import Favourites from "./pages/Favorites/Favorites";
+import NotFound from "./components/NotFound/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import Favorites from "./pages/Favorites/Favorites";
+import Details from "./pages/Details/Details";
+import Navbar from "./components/Header/Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="container-xxl border border-danger d-flex flex-column mh-100">
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" index={true} element={<Home/>}/>
+                    <Route path="/favorites" element={
+                        <ProtectedRoute
+                            element={<Favorites/>}
+                            isLoggedIn={true} //todo: zameni sa pravom vrednoscu
+                        />
+                    }/>
+                    <Route path="/details/:symbol" element={<Details/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,5 @@
+export default function Favourites() {
+    return (
+        <h1>This is a favourites page</h1>
+    );
+}
